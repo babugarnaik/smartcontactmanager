a18ec113e61c85b7a91534f2c6ffe2eb9f1682f1
Subhojitgh <142899317+Subhojitgh@users.noreply.github.com>
2023-12-27 23:03:20 +0530

    Committing Dto and Entity Classes for Review

---
+++ smartcontactmanager/src/main/java/com/scm/smartcontactmanager/controller/category/response/CategoryResponseDto.java
@@ -0,0 +1,20 @@
+package com.scm.smartcontactmanager.controller.category.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryResponseDto {
+	
+	private String name;
+	private String type;
+	private int contactId;
+	private int userId;
+	private boolean byAdmin;
+
+}
