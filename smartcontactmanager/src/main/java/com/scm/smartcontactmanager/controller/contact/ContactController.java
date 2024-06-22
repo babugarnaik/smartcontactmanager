4885049cce122f372e191defa98e21c47d9edc8f
babugarnaik <39863058+babugarnaik@users.noreply.github.com>
2024-06-22 14:00:55 +0530

    Commiting changes

--- smartcontactmanager/src/main/java/com/scm/smartcontactmanager/controller/contact/ContactController.java
+++ smartcontactmanager/src/main/java/com/scm/smartcontactmanager/controller/contact/ContactController.java
@@ -14,14 +14,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
 import com.scm.smartcontactmanager.controller.contact.request.ContactRequestDto;
 import com.scm.smartcontactmanager.entity.contact.Contact;
 import com.scm.smartcontactmanager.service.contact.ContactService;
 
-import jakarta.websocket.server.PathParam;
 
 @RestController
 public class ContactController {
